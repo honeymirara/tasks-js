480479b7abdeabfa0de92e0526f21295d030a621
honeymirara <honeymirara@gmail.com>
2022-12-12 19:48:39 +0300

    Hschool

--- number/16/app.js
+++ number/16/app.js
@@ -1,8 +1,9 @@
 // Пользователь вводит с клавиатуры число. Найдите корень введенного числа (если
 //     же результат – число с плавающей точкой, то округлить до 2 знаков после запятой)
 
-const a = prompt();
+const a = +prompt();
+const sqrt = +Math.sqrt(a);
 
-if  (Number.isInteger(a)) {
-    console.log(Math.sqrt(a));
-} else console.log(Math.exp(a));
+if (Number.isInteger(sqrt)) {
+    console.log(sqrt);
+} else console.log(sqrt.toFixed());
--- number/19/app.js
+++ number/19/app.js
@@ -3,7 +3,23 @@
 // ax
 // 2 + bx + c = 0
 // Если уравнение имеет два корня, то следует вывести их в порядке возрастания.
-const a = +prompt();
+const a = 2;
 const b = +prompt();
-const c = +prompt();
+const c = 4;
+
+const d = b ** 2 - 4 * a * c
+
+if (d < 0) console.log('Корней нет');
+else if (d === 0) console.log(-b / (2 * a));
+else {
+    const x1 = (-b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-b + Math.sqrt(d)) / (2 * a);
+
+    if (x1 > x2) {
+        console.log(x1, x2);
+    } else {
+        console.log(x2, x1);
+    }
+}
+
 
